2dba2078b43c19e136a1b05dd7ab248abe6a7c4c
sli9 <sil9gosy@mail.ru>
2024-04-28 23:30:03 +0300

    added authorization with API-KEY

--- index.js
+++ index.js
@@ -1,60 +1,73 @@
-import express from 'express'
-import morgan from 'morgan'
-import {createProxyMiddleware} from 'http-proxy-middleware'
-import dotenv from 'dotenv'
-import axios from "axios";
-import cors from "cors";
+import express from 'express';
+import cors from 'cors';
+import proxy from 'express-http-proxy';
+import requestIp from 'request-ip';
+import { LRUCache } from 'lru-cache';
+import dotenv from 'dotenv';
+dotenv.config();
 
-dotenv.config()
-
-const app = express()
-
-const HOST = 'localhost'
-const BASE_URL = process.env.API_BASE_URL
-const API_KEY = process.env.API_KEY_VALUE
-const port = process.env.PORT || 3000
-
-app.use(morgan('dev'))
-app.use(cors({
-    origin: true,
-}));
-
-
-const fetchData = async () => {
-    try {
-        return await axios.get(BASE_URL, {
-            headers: {'Authorization': `Bearer ${API_KEY}`}
-        });
-    } catch (error) {
-        console.error(`Error fetching data: ${error}`);
-    }
-};
-
-app.get('/', async (req, res, next) => {
-    const response = await fetchData()
-    res.send(response)
-})
-
-// index.use('', (req, res, next) => {
-//     if (req.headers.authorization) {
-//         next();
-//     } else {
-//         res.sendStatus(403);
-//     }
-// });
-
-app.use('/api.themoviedb.org/3/discover/movie', createProxyMiddleware({
-    target: BASE_URL,
-    changeOrigin: true,
-    pathRewrite: {
-        [`^/api.themoviedb.org/3/discover/movie`]: '',
-    },
-    headers: {
-        'Authorization': `Bearer ${API_KEY}`
-    }
-}));
-
-app.listen(port, HOST, () => {
-    console.log(`Starting Proxy at ${HOST}:${port}`);
+const app = express();
+const port = process.env.PORT || 3001;
+const cookiePerIP = new LRUCache({
+    max: 1000,
+    // how long to live in ms
+    ttl: 1000 * 60 * 3, // 3 min
+    updateAgeOnGet: false,
+    updateAgeOnHas: false,
 });
 
+app.use(
+    cors({
+        origin: true,
+    }),
+);
+
+app.use(requestIp.mw());
+
+app.use(
+    '/',
+    proxy(process.env.ORIGINAL_SERVER_URL, {
+        https: true,
+        headers: {Authorization: `Bearer ${process.env.API_KEY_VALUE}`},
+        userResHeaderDecorator(headers, userReq, userRes, proxyReq, proxyRes) {
+            const key = userReq.clientIp;
+
+            if (headers['set-cookie']) {
+                const newCookies = headers['set-cookie'].map((c) => {
+                    const [key, value] = c.split(';')[0].split('=');
+                    return { key, value };
+                });
+
+                const previousCookies = cookiePerIP.get(key) || [];
+                const currentCookies = previousCookies.concat(newCookies);
+
+                cookiePerIP.set(key, currentCookies);
+            }
+
+            return headers;
+        },
+        proxyReqOptDecorator: function (proxyReqOpts, srcReq) {
+            const key = srcReq.clientIp;
+
+            if (cookiePerIP.has(key)) {
+                const cookies = cookiePerIP
+                    .get(key)
+                    .map((c) => `${c.key}=${c.value}`)
+                    .join(';');
+
+                proxyReqOpts.headers['cookie'] = cookies;
+            }
+
+            return proxyReqOpts;
+        },
+    }),
+);
+
+app.use((err, _req, res, _next) => {
+    console.error(err.stack);
+    res.status(500).send('Server error');
+});
+
+app.listen(port, () => {
+    console.log('Server started');
+});
