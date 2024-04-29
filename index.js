813b52fc97042eb1935c5be85556447dc6523857
sli9 <sil9gosy@mail.ru>
2024-04-29 20:22:32 +0300

    new proxy function

--- index.js
+++ index.js
@@ -1,73 +1,19 @@
 import express from 'express';
-import cors from 'cors';
 import proxy from 'express-http-proxy';
-import requestIp from 'request-ip';
-import { LRUCache } from 'lru-cache';
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3001;
-const cookiePerIP = new LRUCache({
-    max: 1000,
-    // how long to live in ms
-    ttl: 1000 * 60 * 3, // 3 min
-    updateAgeOnGet: false,
-    updateAgeOnHas: false,
-});
 
-app.use(
-    cors({
-        origin: true,
-    }),
-);
-
-app.use(requestIp.mw());
-
-app.use(
-    '/',
-    proxy('https://api.themoviedb.org/3/discover/movie', {
-        https: true,
-        changeOrigin: true,
-        userResHeaderDecorator(headers, userReq, userRes, proxyReq, proxyRes) {
-            const key = userReq.clientIp;
-
-            if (headers['set-cookie']) {
-                const newCookies = headers['set-cookie'].map((c) => {
-                    const [key, value] = c.split(';')[0].split('=');
-                    return { key, value };
-                });
-
-                const previousCookies = cookiePerIP.get(key) || [];
-                const currentCookies = previousCookies.concat(newCookies);
-
-                cookiePerIP.set(key, currentCookies);
-                cookiePerIP.set("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNjQ1OGU3NzVlNDYyOWQ0NzI4ZTQ1OGIzMjI0Y2ZhYyIsInN1YiI6IjY2Mjk1OTNkMzc4MDYyMDE3ZWRhYWY2YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.f5CiN-1oMWrBItC32Oq7M382c_iUffyWk02LvZzx5Xo")
-            }
-
-            return headers;
-        },
-        proxyReqOptDecorator: function (proxyReqOpts, srcReq) {
-            const key = srcReq.clientIp;
-
-            if (cookiePerIP.has(key)) {
-                const cookies = cookiePerIP
-                    .get(key)
-                    .map((c) => `${c.key}=${c.value}`)
-                    .join(';');
-
-                proxyReqOpts.headers['cookie'] = cookies;
-            }
-
-            return proxyReqOpts;
-        },
-    }),
-);
-
-app.use((err, _req, res, _next) => {
-    console.error(err.stack);
-    res.status(500).send('Server error');
-});
+app.use('/proxy', proxy('https://api.themoviedb.org/3/discover/movie', {
+    changeOrigin: true,
+    headers: {
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNjQ1OGU3NzVlNDYyOWQ0NzI4ZTQ1OGIzMjI0Y2ZhYyIsInN1YiI6IjY2Mjk1OTNkMzc4MDYyMDE3ZWRhYWY2YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.f5CiN-1oMWrBItC32Oq7M382c_iUffyWk02LvZzx5Xo',
+        accept: 'application/json'
+    }
+}))
 
 app.listen(port, () => {
     console.log('Server started');
