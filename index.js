dd70f04d0ff79d08864cf81e450fa36bc96bb5be
sli9 <sil9gosy@mail.ru>
2024-04-29 21:57:52 +0300

    add

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3001;
 app.use(cors())
 
 
-app.use('/api/*', proxy('https://api.themoviedb.org/3/discover/movie?api_key=26458e775e4629d4728e458b3224cfac', {
+app.use('/api/*', proxy('https://themoviedb.org/3/discover/movie?api_key=26458e775e4629d4728e458b3224cfac', {
     changeOrigin: true,
     headers: {
         accept: 'application/json'
