557a52de479eb3c7aa40ef11fc282b74457e818f
sli9 <sil9gosy@mail.ru>
2024-04-29 21:51:18 +0300

    add cors

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import proxy from 'express-http-proxy';
 import dotenv from 'dotenv';
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3001;
+app.use(cors())
+
 
 app.use('/api/*', proxy('https://api.themoviedb.org/3/discover/movie?api_key=26458e775e4629d4728e458b3224cfac', {
     changeOrigin: true,
