4f62fcf56831490873a03f063d2f2b125c32f9a8
sli9 <sil9gosy@mail.ru>
2024-04-30 17:34:14 +0300

    trying proxy by axios

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
-import proxy from 'express-http-proxy';
 import dotenv from 'dotenv';
 import cors from "cors";
+import axios from "axios";
 
 dotenv.config();
 
@@ -9,13 +9,28 @@ const app = express();
 const port = process.env.PORT || 3001;
 app.use(cors())
 
+app.get('/', (req, res) => {
+    res.send('hi')
+})
 
-app.use('/api/*', proxy('https://themoviedb.org/3/discover/movie?api_key=26458e775e4629d4728e458b3224cfac', {
-    changeOrigin: true,
-    headers: {
-        accept: 'application/json'
-    }
-}))
+
+// app.use('/movie', proxy('https://themoviedb.org/3/discover/movie', {
+//     changeOrigin: true,
+//     headers: {
+//         accept: 'application/json',
+//         Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNjQ1OGU3NzVlNDYyOWQ0NzI4ZTQ1OGIzMjI0Y2ZhYyIsInN1YiI6IjY2Mjk1OTNkMzc4MDYyMDE3ZWRhYWY2YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.f5CiN-1oMWrBItC32Oq7M382c_iUffyWk02LvZzx5Xo'
+//     }
+// }))
+
+app.get('/movie', (req, res) => {
+    axios.get('https://themoviedb.org/3/discover/movie', {headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNjQ1OGU3NzVlNDYyOWQ0NzI4ZTQ1OGIzMjI0Y2ZhYyIsInN1YiI6IjY2Mjk1OTNkMzc4MDYyMDE3ZWRhYWY2YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.f5CiN-1oMWrBItC32Oq7M382c_iUffyWk02LvZzx5Xo'
+
+        }})
+        .then(response => response.data.json())
+        .then(response => res.send(response))
+})
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
