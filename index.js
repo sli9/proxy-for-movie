0c1e21c3e3c2086a62a941155db0e7907a9818eb
sli9 <sil9gosy@mail.ru>
2024-04-30 23:38:12 +0300

    session_id

--- index.js
+++ index.js
@@ -10,14 +10,19 @@ const apiKey = '26458e775e4629d4728e458b3224cfac';
 // Function to forward request with TMDB API key
 const tmdbRequest = async (req, res) => {
     const tmdbUrl = `https://api.themoviedb.org/3${req.url}`; // Build TMDB API url
-    const params = { api_key: apiKey, ...req.query }; // Include API key and query params
+    const params = {api_key: apiKey, ...req.query}; // Include API key and query params
     console.log(params)
     try {
-        const response = await axios.get(tmdbUrl, { params }); // Make request to TMDB
-        res.json(response.data); // Forward response data to client
+        const response = await axios.get(tmdbUrl, {params});
+        if (response.data.request_token) {
+            const token = response.data.request_token
+            const session = await axios.get(`https://www.themoviedb.org/authenticate/${token}`)
+            res.json(session.data)
+        }
+            res.json(response.data); // Forward response data to client
     } catch (error) {
         console.error(error);
-        res.status(500).json({ message: 'Error fetching data from TMDB' });
+        res.status(500).json({message: 'Error fetching data from TMDB'});
     }
 };
 
