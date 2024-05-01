547c8dbcb58215a3268811b8077cc04c1d93a300
sli9 <sil9gosy@mail.ru>
2024-05-01 18:31:36 +0300

    tried responseType a stream for send image

--- routers/image-router.js
+++ routers/image-router.js
@@ -6,8 +6,12 @@ export const imageRouter = Router({})
 const imageRequest = async (req, res) => {
     const imgUrl = `https://image.tmdb.org${req.originalUrl}`
     try {
-        const response = await axios.get(imgUrl)
-        res.header('content-type', response.headers['content-type']).send(response.data)
+        const response = await axios.get(imgUrl, {responseType: 'stream'})
+
+        res.set({
+            'Content-Type': response.headers['content-type']
+        })
+        response.data.pipe(res)
     }
     catch (error) {
         res.status(500).json({ message: 'Error fetching image from TMDB' })
