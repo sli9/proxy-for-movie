99d880b1a6f829870e851615dd1adcbcc698a4ec
sli9 <sil9gosy@mail.ru>
2024-05-01 16:47:47 +0300

    added discover-router

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import axios from "axios"
 import cors from 'cors'
+import {discoverRouter} from "./routers/discover-router.js";
 
 const app = express();
 const port = process.env.PORT || 3000; // Set your desired port
@@ -8,23 +9,6 @@ cors({
     origin: true
 })
 
-// Replace with your actual TMDB API key
-const apiKey = '26458e775e4629d4728e458b3224cfac';
-
-// Function to forward request with TMDB API key
-const tmdbRequest = async (req, res) => {
-    const tmdbUrl = `https://api.themoviedb.org${req.originalUrl}`; // Build TMDB API url
-    const params = { api_key: apiKey, ...req.query }; // Include API key and query params
-    console.log(params)
-    try {
-        const response = await axios.get(tmdbUrl, {params});
-            res.json(response.data)
-        }// Forward response data to client
-     catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Error fetching data from TMDB' });
-    }
-};
 
 const imageRequest = async (req, res) => {
     const imgUrl = `https://image.tmdb.org/t/p${req.url}`
@@ -38,7 +22,7 @@ const imageRequest = async (req, res) => {
 }
 
 // Proxy any request to the TMDB API endpoint
-app.use('/', tmdbRequest);
+app.use('/discover', discoverRouter);
 app.use('/image', imageRequest)
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
---
+++ routers/discover-router.js
@@ -0,0 +1,23 @@
+import {Router} from "express";
+import axios from "axios";
+
+const apiKey = '26458e775e4629d4728e458b3224cfac';
+
+export const discoverRouter = Router({})
+
+const tmdbRequest = async (req, res) => {
+    const tmdbUrl = `https://api.themoviedb.org/3${req.originalUrl}`; // Build TMDB API url
+    const params = { api_key: apiKey, ...req.query }; // Include API key and query params
+    console.log(params)
+    try {
+        const response = await axios.get(tmdbUrl, {params});
+        res.json(response.data)
+    }// Forward response data to client
+    catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error fetching data from TMDB' });
+    }
+};
+
+discoverRouter.get('/movie', tmdbRequest)
+
