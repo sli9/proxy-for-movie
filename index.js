ab448bce2e766dfa8885f4cb4aefe9a51a3d249d
sli9 <sil9gosy@mail.ru>
2024-05-01 17:03:03 +0300

    added image-router

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import express from "express";
-import axios from "axios"
 import cors from 'cors'
 import {discoverRouter} from "./routers/discover-router.js";
 
@@ -8,18 +7,6 @@ const port = process.env.PORT || 3000; // Set your desired port
 cors({
     origin: true
 })
-
-
-const imageRequest = async (req, res) => {
-    const imgUrl = `https://image.tmdb.org/t/p${req.url}`
-    try {
-        const response = await axios.get(imgUrl)
-        res.json(response.data)
-    }
-    catch (error) {
-        res.status(500).json({ message: 'Error fetching image from TMDB' })
-    }
-}
 
 // Proxy any request to the TMDB API endpoint
 app.use('/discover', discoverRouter);
---
+++ routers/image-router.js
@@ -0,0 +1,17 @@
+import {Router} from "express";
+import axios from "axios";
+
+export const imageRouter = Router({})
+
+const imageRequest = async (req, res) => {
+    const imgUrl = `https://image.tmdb.org/t/p${req.path}`
+    try {
+        const response = await axios.get(imgUrl)
+        res.json(response.data)
+    }
+    catch (error) {
+        res.status(500).json({ message: 'Error fetching image from TMDB' })
+    }
+}
+
+imageRouter.get('/', imageRequest)
