3e13704fe352d2ed28053adade841326b0cff617
sli9 <sil9gosy@mail.ru>
2024-05-01 17:17:12 +0300

    added import imageRouter

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from 'cors'
 import {discoverRouter} from "./routers/discover-router.js";
+import {imageRouter} from "./routers/image-router.js";
 
 const app = express();
 const port = process.env.PORT || 3000; // Set your desired port
@@ -10,6 +11,6 @@ cors({
 
 // Proxy any request to the TMDB API endpoint
 app.use('/discover', discoverRouter);
-app.use('/image', imageRequest)
+app.use('/image', imageRouter)
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
