3624326f742b1cc9974c96ab0594fed8a6ee87df
sli9 <sil9gosy@mail.ru>
2024-05-01 18:25:22 +0300

    changed response headers in the imageRequest

--- routers/image-router.js
+++ routers/image-router.js
@@ -7,7 +7,7 @@ const imageRequest = async (req, res) => {
     const imgUrl = `https://image.tmdb.org${req.originalUrl}`
     try {
         const response = await axios.get(imgUrl)
-        res.sendFile(response.data)
+        res.header('content-type', response.headers['content-type']).send(response.data)
     }
     catch (error) {
         res.status(500).json({ message: 'Error fetching image from TMDB' })
